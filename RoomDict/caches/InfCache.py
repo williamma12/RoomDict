1c622e51205c2ab7d883ef00bab5bc882954740f
William Ma <12377941+williamma12@users.noreply.github.com>
2021-02-11 15:46:21 -0600

    Add infinite cache
    Signed-off-by: William Ma <12377941+williamma12@users.noreply.github.com>

---
+++ RoomDict/caches/InfCache.py
@@ -0,0 +1,28 @@
+from collections.abc import Iterable
+from typing import Optional, Union
+
+from RoomDict.caches.GenericCache import GenericCache, Record
+
+class InfCache(GenericCache):
+    def __init__(self):
+        self.directory: dict[str, object] = {}
+        
+        super().__init__()
+
+    def put(self, key: str, value: object):
+        self.directory[key] = value
+
+    def get(self, key: str) -> Record:
+        value = self.directory[key]
+
+        return Record(key, value)
+
+    def __delitem__(self, key: str):
+        del self.directory[key]
+
+    def __iter__(self) -> Iterable[Union[str, object]]:
+        return iter(self.directory)
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.directory
+

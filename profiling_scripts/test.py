059e8cd4e79f22a320e97c54875e6f788cdc4b1d
William Ma <12377941+williamma12@users.noreply.github.com>
2021-03-03 20:45:23 -0600

    update profiling scripts
    Signed-off-by: William Ma <12377941+williamma12@users.noreply.github.com>

--- profiling_scripts/test.py
+++ profiling_scripts/test.py
@@ -6,7 +6,7 @@ membership_tests = ["none", "none"]
 storage_backends = ["memory", "arbitrary"]
 
 cache_policies_kwargs = [{"max_size": 3}]
-storage_backends_kwargs = [{}, {"delay": 100}]
+storage_backends_kwargs = [{}, {"delay": 1}]
 
 with RoomDict(cache_policies, membership_tests, storage_backends, cache_policies_kwargs, storage_backends_kwargs=storage_backends_kwargs) as cache:
     for i in range(10):
